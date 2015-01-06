7a1ae5ec62a0162887f94b86628b5b229b0ee5f0
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-06 13:37:41 -0500

    Add new module to use sqlite
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

---
+++ dbmodule.lua
@@ -0,0 +1,21 @@
+-- db module
+
+local dbmodule = {}
+
+function dbmodule.CreateDB( ... )
+  -- body
+end
+
+function dbmodule.CreateTable( ... )
+  -- body
+end
+
+function dbmodule.InsertScript( ... )
+  -- body
+end
+
+function dbmodule.UpdateScript( ... )
+  -- body
+end
+
+return dbmodule
