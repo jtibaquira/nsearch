7b1e5ab192f4050796ae13db3917182d70630931
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-07 13:37:52 -0500

    Add banner
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ local config = {}
 
 config.filePath = '/usr/share/nmap/scripts/script.db'
 config.fileBackup = 'scriptbkp.db'
-config.scriptdb = 'script.sqlite3'
+config.scriptdb = 'nmap_scripts.sqlite3'
 config.categories = {
                       "auth","broadcast","brute","default","discovery","dos","exploit",
                       "external","fuzzer","intrusive","malware","safe","version","vuln"
