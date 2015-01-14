a34af4331f4e7f4a542ae7baf2c689694d9b8d05
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-14 10:45:51 -0500

    Add fixing new file for instalation
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-#!/bin/env bash
+#!/bin/bash
 
-dbpath=$(find /usr -type f -name "script.db" 2>/dev/null)
+dbpath=$(find /usr -type f -name "script.db" 2>/dev/null | awk 'gsub("script.db","")')
 
 if [[ $dbpath ]]; then
-  echo $dbpath >> settings.lua
+  echo $dbpath
+  echo -e "local settings = {} \n" > settings.lua
+  echo -e "settings.scriptsPath='$dbpath'" >> settings.lua
+  echo -e "settings.filePath = settings.scriptsPath..'script.db'" >> settings.lua
+  echo -e "settings.fileBackup = 'scriptbkp.db'" >> settings.lua
+  echo -e "settings.scriptdb = 'nmap_scripts.sqlite3'" >> settings.lua
+  echo -e 'settings.categories = {"auth","broadcast","brute","default","discovery","dos","exploit","external","fuzzer","intrusive","malware","safe","version","vuln"}\n' >> settings.lua
+  echo -e "return settings" >> settings.lua
 fi
