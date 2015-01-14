eaa3ebbe6046443d22f70da623acf766596c4288
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-14 17:28:29 -0500

    Fixing os detection
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 
+nmapversion=$(nmap -V 2>/dev/null)
+luaversion=$(lua -v 2>/dev/null)
+luarocks=$(luarocks 2>/dev/null)
+
+if [[ $nmapversion ]]; then
+  echo "Nmap Installed"
+  $nmapversion
+else
+  while true; do
+    read -p "Do you wish to install nmap?" yn
+    case $yn in
+      [Yy]* ) make install; break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi
+
+if [[ $luaversion ]]; then
+  echo "Lua Installed"
+  $luaversion
+else
+  while true; do
+    read -p "Do you wish to install lua?" yn
+    case $yn in
+      [Yy]* ) make install; break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi
+
+if [[ $luarocks ]]; then
+  echo "Lua rocks installed"
+  $luarocks
+else
+  while true; do
+    read -p "Do you wish to install luarocks?" yn
+    case $yn in
+      [Yy]* ) make install; break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi
+
+
+<<COMMENT1
 dbpath=$(find /usr -type f -name "script.db" 2>/dev/null | awk 'gsub("script.db","")')
 nmappath=$(find /usr -type f -name "nmap" 2>/dev/null)
 luapath=$(find /usr -type f -name "lua" 2>/dev/null)
@@ -40,8 +88,6 @@ if [ -f /etc/lsb-release ]; then
   . /etc/lsb-release
   OS=$DISTRIB_ID
   VER=$DISTRIB_RELEASE
-  echo $VER
-  echo $OS
 elif [ -f /etc/debian_version ]; then
   OS=Debian  # XXX or Ubuntu??
   VER=$(cat /etc/debian_version)
@@ -51,3 +97,4 @@ else
   OS=$(uname -s)
   VER=$(uname -r)
 fi
+COMMENT1
