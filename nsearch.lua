94d52b1ea03c56dce99dc1d5bf72bd40785ca350
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-25 14:43:58 -0500

    Refactoring Setup Action

--- nsearch.lua
+++ nsearch.lua
@@ -3,7 +3,9 @@
 local setup = require "setup"
 local dbmodule = require "dbmodule"
 local config = require "config"
-scriptdb = config.scriptdb
+local categoryList = config.categories
+local filePath = config.filePath
+local scriptdb = config.scriptdb
 
 
 local banner=[[
@@ -20,7 +22,68 @@ local banner=[[
   ]]
 
 
-function returnSearchConsole()
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then print "File don't exist" os.exit() end
+  lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+-- tests the functions above
+local file = filePath
+local lines = lines_from(file)
+
+-- create a script.db backups
+function createBackup()
+  outfile = io.open(config.fileBackup, "w")
+  for k,v in pairs(lines) do
+    outfile:write(v.."\n")
+  end
+  outfile:close()
+  if not file_exists(config.fileBackup) then
+    print "the backup can not created"
+  else
+    print "backup succesfull"
+  end
+end
+
+function install()
+  dbmodule.InitSetup("wc")
+  local t ={}
+  local id_script = 0
+  for k,v in ipairs(lines) do
+    v = v:gsub('%Entry { filename = "',""):gsub('", categories = { "',',"'):gsub('", } }','"'):gsub('", "','","')
+    for i,c in ipairs(categoryList) do
+      v = v:gsub('"'..c..'"',i)
+    end
+    for a in string.gmatch(v,"([^,]+)") do
+      table.insert(t,a)
+    end
+    for key,value in ipairs(t) do
+      if t[1] == value then
+        local val = {value}
+        id_script = dbmodule.InsertScript(val,"scripts")
+      else
+        dbmodule.InsertCategory(id_script,value)
+      end
+    end
+    t = {}
+  end
+  createBackup()
+end
+
+
+function returnConsole()
   os.execute("clear")
   print('\27[1m \27[36m'..banner..'\27[21m \27[0m')
   searchConsole()
@@ -43,9 +106,9 @@ function getScriptDesc( nse )
       end
     end
   elseif option == "0" then
-    returnSearchConsole()
+    returnConsole()
   else
-    returnSearchConsole()
+    returnConsole()
   end
 end
 
@@ -63,9 +126,9 @@ function resultList(nse)
     io.write("Do you want search again? [y/n]: ")
     local action = io.read()
     if action == 'y' then
-      returnSearchConsole()
+      returnConsole()
     else
-      returnSearchConsole()
+      returnConsole()
     end
    end
 end
@@ -87,7 +150,7 @@ end
 
 
 function searchConsole()
-  if not setup.file_exists(config.scriptdb) then setup.install() end
+  if not file_exists(config.scriptdb) then install() end
   io.write('nsearch> ')
   local command = io.read()
   if command == "help" then
@@ -112,7 +175,7 @@ function searchConsole()
     os.exit()
   elseif string.find(command,"clear") then
     os.execute("clear")
-    returnSearchConsole()
+    returnConsole()
   elseif string.find(command,"category:") then
     string = command:gsub("category:","")
     os.execute("clear")
@@ -137,7 +200,7 @@ end
 
 -- validation of args
 if countArgs() < 1 then
-  returnSearchConsole()
+  returnConsole()
 else
   helpMenu()
 end
