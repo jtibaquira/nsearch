48b225632a3e4bf889a8024d610700078eb31a19
Jacobo Tibaquira <jacobotibaquira@gmail.com>
2015-01-28 14:33:55 -0500

    Add new fucntions for console actions
    Signed-off-by: Jacobo Tibaquira <jacobotibaquira@gmail.com>

--- nsearch.py
+++ nsearch.py
@@ -4,6 +4,7 @@
 import yaml
 import dbmodule
 import os
+import sys
 
 stream = open("config.yaml", 'r')
 item = yaml.load(stream)
@@ -27,6 +28,11 @@ banner ='''
   ================================================
 '''
 
+def main():
+  os.system("clear")
+  print('\033[0;36m'+banner+'\033[0m')
+  mainConsole()
+
 def createBackUp():
   print("Creating Script.db Backup ... ")
   scriptFile = open(filePath,'r')
@@ -42,24 +48,49 @@ def createBackUp():
 
 
 def install():
-  print('\033[0;36m'+banner+'\033[0m')
+  dbmodule.initSetup(dbname, categories)
+  scriptFile = open(filePath,'r')
+  for line in scriptFile:
+    line = line.replace('Entry { filename = "',"").replace('", categories = { "',',"').replace('", } }','"').replace('", "','","')
+    for i, j in enumerate(categories):
+      line = line.replace('"'+j+'"',str(i+1))
+    newarray = line.split(",")
+    for key,value in enumerate(newarray):
+      if value == newarray[0]:
+        lastrowid = dbmodule.insertScript(dbname,value)
+      else:
+        dbmodule.insertScriptCategory(dbname,lastrowid,value)
+  scriptFile.close()
+  createBackUp()
+
+
+def mainConsole():
   if not os.path.isfile(dbname):
-    dbmodule.initSetup(dbname, categories)
-    scriptFile = open(filePath,'r')
-    for line in scriptFile:
-      line = line.replace('Entry { filename = "',"").replace('", categories = { "',',"').replace('", } }','"').replace('", "','","')
-      for i, j in enumerate(categories):
-        line = line.replace('"'+j+'"',str(i+1))
-      newarray = line.split(",")
-      for key,value in enumerate(newarray):
-        if value == newarray[0]:
-          lastrowid = dbmodule.insertScript(dbname,value)
-        else:
-          dbmodule.insertScriptCategory(dbname,lastrowid,value)
-    scriptFile.close()
-    createBackUp()
+    install()
+  is_valid=0
+  while not is_valid :
+    try :
+      command = raw_input('nsearch> ')
+      is_valid = 1 ## set it to 1 to validate input and to terminate the while..not loop
+    except ValueError, e :
+      print ("'%s' is not a valid command." % e.args[0].split(": ")[1])
+      mainConsole()
+  if command == "help":
+    print("\tname     : Search by script's name")
+    print("\tcategory : Search by category")
+    print("\texit     : Close the console")
+    print("\tclear    : Clean the console")
+    print("\n\t Usage:")
+    print("\t   name:http")
+    print("\t   category:exploit \n")
+    mainConsole()
+  elif command == "exit":
+    sys.exit(0)
+  elif command == "clear":
+    os.system("clear")
+    main()
   else:
-    print("Exist: "+ dbname)
+    mainConsole()
 
-
-install()
+if __name__ == "__main__":
+  main()
